80ddad59b29b11c7ef27438aa5539d997952a0b1
Anderson <ped2.nexsolar@gmail.com>
2024-08-25 00:34:43 -0400

    Layout com o NavBAr e Footer pronto

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
+import {Hero} from "@/components";
 import Image from "next/image";
-import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
-      <Hero />
+      <Hero/>
     </main>
   );
 }
